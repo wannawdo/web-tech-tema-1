df0d1c55ccf905a8a109c9958fb99ee6ed90bfb2
Oana Dragomirescu <dragomirescuoana18@stud.ase.ro>
2020-10-31 16:25:06 +0200

    Adaugat tema 1 - Dragomirescu Oana,1080

--- main/app.js
+++ main/app.js
@@ -1,6 +1,21 @@
 function removeOrderItem(orderInfo, position){
-    
+    if(!Array.isArray(orderInfo.items))
+    throw "Items should be an array"
+    orderInfo.items.forEach(element => {
+        if(!element.price && !element.quantity)
+        throw "Malformed item"
+    });
+    if(position>=orderInfo.items.length)
+    throw "Invalid position"
+    else
+    {
+        const found = orderInfo.items[position];
+        orderInfo.items.splice(position, 1);
+        orderInfo.total = orderInfo.total - found.price * found.quantity;
+    }
+    return orderInfo
 }
+
 
 const app = {
     removeOrderItem
